e0d36454a5e219a483b2e7fed3c0457a2e845049
Wn0fbg <temakozlovofficiall@gmail.com>
2026-05-26 20:54:27 +0300

    restart shop page

--- components/Shop/Shop.jsx
+++ components/Shop/Shop.jsx
@@ -4,7 +4,10 @@ import "@/app/globals.css";
 const Shop = () => {
   return (
     <div className="container">
-      <div className="filters"></div>
+      <div className="products__container"></div>
+      <div className="filters">
+        <h2 className="filters__title">Filters</h2>
+      </div>
       <div className="products"></div>
     </div>
   );
